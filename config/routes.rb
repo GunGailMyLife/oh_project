e19de6225c77bb80e7eeb2dd70699253a4302e12
HGPMAC 159 <hgpmac159@Thes-MacBook-Air-59.local>
2019-10-18 19:00:07 -0700

    Oct 18,2019 we finnised all of project one

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,15 @@ Rails.application.routes.draw do
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
   resources :guess
+  get '/guess_results1', to: 'guess#fight_results_1'
+  get '/guess_results2', to: 'guess#fight_results_2'
+  get '/guess_results3', to: 'guess#fight_results_3'
+  get '/guess_results4', to: 'guess#fight_results_4'
+  get '/guess_results5', to: 'guess#fight_results_5'
+  get '/guess_results6', to: 'guess#fight_results_6'
+  get '/guess_results7', to: 'guess#fight_results_7'
+  get '/guess_fightpg1', to: 'guess#fight_pg_1'
+  get '/guess_fightpg2', to: 'guess#fight_pg_2'
 
 
 
